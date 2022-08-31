4a2081addb127097d42e1e51a8e424e9d4fe7f7a
Biancaferre <ferreirabianca941@gmail.com>
2022-08-31 20:18:16 +0000

    add2

--- public/script.js
+++ public/script.js
@@ -10,22 +10,22 @@ function setSaldo(saldo) {
 
 function criarTransacao(transacao) {
   const linha = document.createElement('tr');
-  
+
   const descricaoElement = document.createElement('td');
   descricaoElement.setAttribute('class', 'coluna-descricao');
   descricaoElement.append(transacao.descricao);
   linha.append(descricaoElement);
-  
+
   const categoriaElement = document.createElement('td');
   categoriaElement.setAttribute('class', 'coluna-categoria');
   categoriaElement.append(transacao.categoria);
   linha.append(categoriaElement);
-  
+
   const valorElement = document.createElement('td');
   valorElement.setAttribute('class', 'coluna-valor');
   valorElement.append(formatarValor(transacao.valor));
   linha.append(valorElement);
-  
+
   return linha;
 }
 
@@ -37,44 +37,47 @@ function setTransacoes(transacoes) {
     const linha = criarTransacao(transacao);
     tabelaConteudo.append(linha);
   });
-  
+
   document.getElementById('lista-transacoes-conteudo').replaceWith(tabelaConteudo);
 }
 
 async function buscarTransacoes() {
-  const response = await fetch('https://run.mocky.io/v3/ba2007f7-04ea-465b-985e-b16c11e8061d');
+  const response = await fetch('/transacoes');
   const financas = await response.json();
-  
+
   setSaldo(financas.saldo);
   setTransacoes(financas.transacoes);
 }
 
-async function enviarDadosTransacao(descricao, valor) {
+async function enviarDadosTransacao(descricao, valor, categoria) {
   if (valor.indexOf(',') > 0) {
     alert('Você deve digitar números com o símbolo decimal ponto, e não vírgula');
     return;
   }
-  
+
   if (isNaN(valor)) {
     alert('Você deve digitar um número no campo valor!');
     return;
   }
-  
-  const transacao = { descricao, valor: Number(valor) };
-  
+
+  const transacao = { descricao, valor: Number(valor), categoria };
+  console.log('transacao ' + JSON.stringify(transacao))
   const requisicao = {
     method: 'POST',
     body: JSON.stringify(transacao),
+    headers: {
+      "content-type": "application/json"
+    }
   };
-  await fetch('https://run.mocky.io/v3/c50981e7-1c9b-471c-9128-3adf362cf36f', requisicao);
+  await fetch('/transacoes', requisicao);
 }
 
 async function adicionarDespesa() {
   const descricaoDespesa = window.prompt('Qual a descricao de sua despesa?');
   const valorDespesa = window.prompt('Qual o valor de sua despesa?');
 
-  await enviarDadosTransacao(descricaoDespesa, valorDespesa);
-  
+  await enviarDadosTransacao(descricaoDespesa, valorDespesa, "Despesa");
+
   buscarTransacoes();
 }
 
@@ -82,8 +85,8 @@ async function adicionarReceita() {
   const descricaoReceita = window.prompt('Qual a descricao de sua receita?');
   const valorReceita = window.prompt('Qual o valor de sua receita?');
 
-  await enviarDadosTransacao(descricaoReceita, valorReceita);
-  
+  await enviarDadosTransacao(descricaoReceita, valorReceita, "Receita");
+
   buscarTransacoes();
 }
 
